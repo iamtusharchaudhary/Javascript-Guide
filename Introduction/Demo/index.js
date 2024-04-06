a2d88f3929471764cfbc3146b6e9da135861aee3
Tushar Chaudhary <149557229+iamtusharchaudhary@users.noreply.github.com>
2024-04-06 13:38:04 +0530

    Update index.js

--- Introduction/Demo/index.js
+++ Introduction/Demo/index.js
@@ -1,1 +1,46 @@
+1.  This Js file is attached in your html file then you give your output.
+2.  In my html file this file is already attached at bottom.  
 
+const obj={
+    name: "tushar",
+    course: "Full Stack",
+    date: 2/10/2023,
+}
+console.log(typeof obj);
+console.log(obj.course);
+
+// object 2nd pattern:
+const data={}
+data.a1=31;
+data.a2="tushar";
+console.log(typeof data);
+
+// object 3rd pattern:
+const tata={
+    "uid":"tushar",
+    "num":55,
+}
+console.log(tata.uid);
+
+// function 1st pattern:
+function name(){
+    return "welcome to javascript"
+}
+console.log(name());
+
+// function 2nd pattern:
+const app=function tata1(){
+    return "welcome to js"
+}
+console.log(app());
+
+// function 3rd pattern
+const takla=()=>"welcome to java script"
+console.log(takla());
+
+// Array
+let hnji=[22,33,55,67,89,909,0]
+console.log(typeof hnji);
+//ffrhg
+let number= '1 2 4 6 7 8 9 77 8 9';
+console.log(number);
