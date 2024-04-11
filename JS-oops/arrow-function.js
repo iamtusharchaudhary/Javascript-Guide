dfeb57f6c7ad79ca70c370ec95be81cbc4379ecc
Tushar Chaudhary <149557229+iamtusharchaudhary@users.noreply.github.com>
2024-04-11 10:19:44 +0530

    Update arrow-function.js

--- JS-oops/arrow-function.js
+++ JS-oops/arrow-function.js
@@ -1,1 +1,20 @@
+// Case 1 :-
 
+function name(){
+    return'Tushar Chaudhary';
+}
+console.log(name);
+
+// Case 2 :-
+
+function name(){
+    return'Tushar Chaudhary';
+}
+console.log(name());
+
+// Case 3 :-
+
+let app = function name(){
+    return"Technical Sir"
+}
+console.log(app());
