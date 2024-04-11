53c57edc0e2fcfb98a596a97c5a6ef96d289baf1
Tushar Chaudhary <149557229+iamtusharchaudhary@users.noreply.github.com>
2024-04-11 10:28:00 +0530

    Create template-literals-example.js

---
+++ JS-oops/template-literals-example.js
@@ -0,0 +1,9 @@
+// Case 1 :-
+
+let temp = "Template String";
+console.log(`Welcome to ${temp}`);
+
+// Case 2:-
+
+let string = "my javascript template literals code"
+console.log(`Welcome to ${string}`);
