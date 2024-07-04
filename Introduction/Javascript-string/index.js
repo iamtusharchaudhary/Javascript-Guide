0af02be7c7a38e58346c1a769d321dcd8c37f61f
Tushar Chaudhary <149557229+iamtusharchaudhary@users.noreply.github.com>
2024-07-04 09:16:23 +0000

    config done

---
+++ Introduction/Javascript-string/index.js
@@ -0,0 +1,15 @@
+
+// First pattern to declare a string
+
+const string = "Tushar";
+console.log(string);
+
+
+// Second and latest method to define a string
+
+const anotherString = new string("Tushar Chaudhary");
+console.log(anotherString);
+
+const anotherString2 = anotherString.substring(0 , 6)
+console.log(anotherString2);  // Output of this anotherSting2 is Tushar and before you run this please comment out above anotherString otherwise your output is given a error.
+
